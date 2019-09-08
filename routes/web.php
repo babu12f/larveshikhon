b239ba3662497f9329714a2d1d4a1734233694bc
Shofiullah Babor <babu_12f@yahoo.com>
2019-09-08 21:33:09 +0600

    laravel auth create class 20 complete

--- routes/web.php
+++ routes/web.php
@@ -3,3 +3,10 @@
 Route::get('/', function(){
     return view('welcome');
 });
+Auth::routes();
+
+Route::get('/home', 'HomeController@index')->name('home');
+
+Route::resource('videos', 'VideosController')->middleware('auth');
+
+
